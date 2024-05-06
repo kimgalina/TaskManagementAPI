02b3a3352c676144b416869d923eefac15d27ed9
kimgalina <genrietta.kim@gmail.com>
2024-05-06 22:48:58 +0600

    Added settings for mail sender bean

--- src/main/java/kg/kim/TaskManagementAPI/config/ApplicationConfig.java
+++ src/main/java/kg/kim/TaskManagementAPI/config/ApplicationConfig.java
@@ -2,9 +2,11 @@ package kg.kim.TaskManagementAPI.config;
 
 import kg.kim.TaskManagementAPI.entity.User;
 import kg.kim.TaskManagementAPI.repository.UserRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,9 +17,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Properties;
 
 @Configuration
-@AllArgsConstructor
 public class ApplicationConfig {
+
     private final UserRepository userRepository;
+
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+
+    public ApplicationConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 
     @Bean
@@ -42,7 +57,23 @@ public class ApplicationConfig {
         return authProvider;
     }
 
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
 
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
+
+        return mailSender;
+    }
 
 }
 
